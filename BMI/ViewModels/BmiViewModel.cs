7f0190a1607d337a2f4af1b65c96b94f3fb862af
Yiling <yll615@ms31.hinet.net>
2021-01-10 21:01:44 +0800

    2021-01-10 加上計算方式和結果顯示

---
+++ BMI/ViewModels/BmiViewModel.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace BMI.ViewModels
+{
+    internal class BmiViewModel : NotifyPropertyBase
+    {
+        public double height;
+
+        public double Height
+        {
+            get { return height; }
+            set { SetProperty(ref height, value); }
+        }
+
+        public double weight;
+
+        public double Weight
+        {
+            get { return weight; }
+            set { SetProperty(ref weight, value); }
+        }
+
+        public double result;
+
+        public double Result
+        {
+            get { return result; }
+            set { SetProperty(ref result, value); }
+        }
+
+        private ICommand submitCommand;
+
+        public ICommand SubmitCommand
+        {
+            get
+            {
+                if (submitCommand == null)
+                {
+                    submitCommand = new RelayCommand((x) =>
+                    this.Result = Math.Round(Weight / Math.Pow((Height / 100), 2), 2)
+                    ); ;
+                }
+                return submitCommand;
+            }
+        }
+    }
+}
